5d3d6e7bab2599ffb9eb5906fcbad15869a86372
Engr-Carlo <carlobisa2000@gmail.com>
2026-02-08 19:05:24 +0800

    Fix middleware matcher regex syntax error

--- middleware.ts
+++ middleware.ts
@@ -44,6 +44,6 @@ export const config = {
      * - _next/image (image optimization files)
      * - favicon.ico, images
      */
-    \"/((?!api|_next/static|_next/image|favicon.ico|.*\\\\.(?:svg|png|jpg|jpeg|gif|webp|ico)).*)\",
+    "/((?!api|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp|ico)).*)",
   ],
 };
